9c38e0f405f653d2606f719a96aee2b79a3036cb
pvanliefland <pierre@snowcap.be>
2013-01-08 18:11:31 +0100

    Adapted to latest changes in Core Bundle (twig extension for pagination)

--- SnowcapBootstrapBundle.php
+++ SnowcapBootstrapBundle.php
@@ -3,7 +3,17 @@
 namespace Snowcap\BootstrapBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Snowcap\BootstrapBundle\DependencyInjection\Compiler\PaginatorCompilerPass;
 
 class SnowcapBootstrapBundle extends Bundle
 {
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new PaginatorCompilerPass());
+    }
 }
