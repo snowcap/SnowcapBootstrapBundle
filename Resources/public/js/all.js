8b116c6f022d62fa67ab3aafa9ab7aacbdc82d18
Jonathan Scheiber <jonathan@snowcap.be>
2013-09-17 16:45:38 +0200

    Fixed modal for "data-confirm" for Bootstrap 3

--- Resources/public/js/all.js
+++ Resources/public/js/all.js
@@ -8,28 +8,45 @@ jQuery(document).ready(function ($) {
     });
 
     // Confirm actions
-    $('*[data-confirm="confirm"]').on('click', function(event) {
+    $('[data-confirm=confirm]').on('click', function(event) {
         event.preventDefault();
         var href = $(this).attr('href');
         var confirmButton = $('<a>').addClass('btn btn-large btn-primary').html($(this).data('confirm_confirm'));
         confirmButton.on('click', function(event) {
             event.preventDefault();
             window.location.href = href;
-        })
-        var modal = $('#modal');
+        });
+        var modal = $('<div>')
+            .addClass('modal fade')
+            .attr({
+                'id': 'modal',
+                'tabindex': '-1',
+                'role': 'dialog',
+                'aria-hidden': 'true'
+            });
+        var modalDialog = $('<div class="modal-dialog"></div>');
+        var modalContent = $('<div class="modal-content"></div>');
         var modalHeader = $('<div>').addClass('modal-header')
-            .append($('<a>').addClass('close').attr('data-dismiss', 'modal').html('x'))
-            .append($('<h3>').html($(this).data('confirm_title')));
-        var modalBody = $('<div>').addClass('modal-header')
+            .append($('<button>').addClass('close').attr({
+                'type': 'button',
+                'data-dismiss': 'modal',
+                'aria-hidden': 'true'
+            }).html('&times;'))
+            .append($('<h4>').html($(this).data('confirm_title')));
+        var modalBody = $('<div>').addClass('modal-body')
             .append($('<p>').html($(this).data('confirm_body')));
         var modalFooter = $('<div>').addClass('modal-footer')
-            .append($('<a>').addClass('btn btn-large').attr('data-dismiss', 'modal').html($(this).data('confirm_cancel')))
+            .append($('<a>').addClass('btn btn-large btn-default').attr('data-dismiss', 'modal').html($(this).data('confirm_cancel')))
             .append(confirmButton);
-        modal.append(modalHeader).append(modalBody).append(modalFooter);
+        modalContent.append(modalHeader).append(modalBody).append(modalFooter);
+        modalDialog.append(modalContent);
+        modal.append(modalDialog);
+
+        $('body').append(modal);
 
         modal.modal('show');
     });
 
     // Tooltips
-    $("*[rel=tooltip]").tooltip().css({'cursor':'pointer'});
+    $("[rel=tooltip]").tooltip().css({'cursor':'pointer'});
 });
